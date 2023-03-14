2230fa88ea7afc034a73fe69bc2a31fab67fb2fe
emolr <emolr@users.noreply.github.com>
2023-03-14 10:13:04 +0100

    1 file modified, 1 added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import { getPWADisplayMode } from '../libs/getPWADisplayMode';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [isPWA, setIsPWA] = useState(false);
+  useEffect(() => {
+    const browserMode = getPWADisplayMode();
+
+    if (browserMode === 'standalone') {
+      setIsPWA(true);
+    }
+  }, []);
   return (
     <div>
+      {isPWA && <h1>IN PWA WOOOO</h1>}
       <a href="https://nextjs-pwa-example-test-app.vercel.app">
         This is external, let me redirect you to original source
       </a>
